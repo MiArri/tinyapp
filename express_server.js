b5004c0d16551e01eb7a4b372d821456ef666deb
Elena Chernikova <lena@acvos.com>
2022-02-21 19:28:57 -0800

    Body-parser and POST request added

--- express_server.js
+++ express_server.js
@@ -3,8 +3,17 @@ const app = express();
 const PORT = 8080; // default port 8080
 //using ejs template
 app.set("view engine", "ejs");
-
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for ( var i = 0; i < characters.length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 6));
+  }
+   return result;
+  }
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -41,6 +50,11 @@ app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
   const longURL = urlDatabase[shortURL]
   res.render("urls_show", { shortURL, longURL });
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 
