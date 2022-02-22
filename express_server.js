78b802a99f1e80b792e839e2ba312d9f2d6a268b
Elena Chernikova <lena@acvos.com>
2022-02-21 16:48:59 -0800

    New route added for short URL

--- express_server.js
+++ express_server.js
@@ -21,6 +21,12 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//route to render urls_show.ejs template
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
